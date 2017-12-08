864386ee99ab7d11ae11190460528ada73dc535b
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2017-12-08 16:06:01 -0600

    move custom strategy to it's own file

--- config/passport.ts
+++ config/passport.ts
@@ -1,8 +1,5 @@
-
-// load all the things we need
-var LocalStrategy   = require('passport-local').Strategy;
-var CustomStrategy = require('passport-custom').Strategy;
-
+// load up role based strategy
+const roleSignupStrategy = require('./rolebasedsignup.ts');
 // load up the user model
 
 var User            = require('../server/models/user.ts').User;
@@ -27,45 +24,8 @@ module.exports = function(passport) {
         });
     });
 
-    var roleSignupStrategy = new CustomStrategy(function(req, done) {
-        process.nextTick(function() {
-            User.findOne({
-                username: req.body.username,
-            }, function(err, user) {
-                if (err) {
-                    return done(err)
-                }
-                if (user) {
-                    console.log('This user is already registered.')
-                    return done(null, false, {
-                        message: 'This user is already registered.'
-                    });
-                } else {
-                    const newUser = new User();
-                    newUser.username = req.body.username;
-                    newUser.password = newUser.generateHash(req.body.password);
-                    // by default, the new user will be an Admin
-                    if (req.body.role && req.body.role !== 'user' && req.body.role !== 'admin') {
-                        return done(null, false, {
-                            message: 'This is not a valid role.'
-                        })
-                    } else if (req.body.role === 'user') {
-                        newUser.role = 'user';
-                    } else {
-                        newUser.role = 'admin';
-                    }
     
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, newUser);
-                    });
-                }
-            })
-        })
-        
-    })
-    
+
     passport.use(roleSignupStrategy);
 
 };

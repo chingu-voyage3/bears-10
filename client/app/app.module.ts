1fbea452eca149179eb769deba428bd490ef4ad4
Larry Faudree <lfaudreejr@hotmail.com>
2017-12-13 01:29:04 -0500

    input only material modules needed

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module'
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SignupComponent } from './pages/signup/signup.component';
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
-import { UserService } from './user.service';
-import { HttpClientModule } from '@angular/common/http';
 import { InventoryComponent } from './pages/inventory/inventory.component';
+
+import { UserService } from './user.service';
+
 
 
 @NgModule({
@@ -27,12 +27,9 @@ import { InventoryComponent } from './pages/inventory/inventory.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatMenuModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    SharedModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]

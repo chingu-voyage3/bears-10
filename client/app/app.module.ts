2dc19742a948d8f08a8d76bb668af8c05a1293d8
Larry Faudree <lfaudreejr@hotmail.com>
2017-12-13 20:43:19 -0500

    remove test script cause hangup with travisci

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-// import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';

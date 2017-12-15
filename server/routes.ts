fa722f4a31ccd24fb8463b71b519f65ccc141611
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2017-12-14 21:01:28 -0600

    create stub for adding item

--- server/routes.ts
+++ server/routes.ts
@@ -2,8 +2,9 @@ module.exports = function(passport, ExtractJWT, jwt, bcrypt, express, User) {
     const router = express.Router();
     const options = { jwtFromRequest: '', secretOrKey: '' };
 
-    const signupUser = require('./actions/signupUser.ts')(User, bcrypt, jwt, options);
-    const loginUser = require('./actions/loginUser.ts')(User, bcrypt, jwt, options);
+    const signupUser = require('./actions/signupUser.ts')(User, bcrypt, jwt, options),
+          loginUser = require('./actions/loginUser.ts')(User, bcrypt, jwt, options),
+          addItem = require('./actions/addItem.ts')(User, bcrypt, jwt, options);
 
     options.jwtFromRequest = ExtractJWT.fromHeader('Authorization');
     options.secretOrKey = process.env.SECRET_JWT_KEY;
@@ -16,7 +17,11 @@ module.exports = function(passport, ExtractJWT, jwt, bcrypt, express, User) {
                 );
 
     router.post('/login',
-                 loginUser,
+                 loginUser
+               );
+
+    router.post('/add_item',
+                 addItem
                );
 
     return router;

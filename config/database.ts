6d9202bae3da261625d1e6b79e5c4075cd38399e
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2017-12-17 13:29:15 -0600

    Revert "Bugfix/wrong password signin"

--- config/database.ts
+++
@@ -1,1 +0,0 @@
-module.exports.url = 'mongodb://' + process.env.DBUSER + ':' + process.env.DBPASSWORD + '@ds033186.mlab.com:33186/ims';
--- config/passport.ts
+++
@@ -1,8 +0,0 @@
-// load up role based strategy
-const roleSignupStrategy = require('../strategies/rolebasedsignup.ts'),
-      CustomStrategy = require('passport-custom').Strategy;
-
-// expose this function to our app using module.exports
-module.exports = function(passport, User) {
-    passport.use(roleSignupStrategy(CustomStrategy, User));
-};

b486849a631d0cc9f5491850af876256eb3423f3
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2018-01-31 16:41:43 -0600

    add admin guard

---
+++ client/app/core/guards/admin.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { UserService } from '../user.service';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  constructor(private user: UserService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+      let checkIfAdmin = false;
+      this.user.isAdmin.subscribe(isAdmin => {
+          checkIfAdmin = isAdmin;
+      });
+      return checkIfAdmin;
+  }
+}

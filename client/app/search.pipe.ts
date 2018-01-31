05f2f3b188c2e8a1aa98a4fb9557aca05159add3
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2018-01-31 15:55:34 -0600

    return empty array if no values are passed into pipe

--- client/app/search.pipe.ts
+++ client/app/search.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
   transform(values: any, searchString: string): any {
-    return values.filter(e => {
+    return values ? values.filter(e => {
       return e.name.includes(searchString)
       || e.description.includes(searchString)
       || e.size.includes(searchString);
-    });
+    }) : [];
   }
 
 }

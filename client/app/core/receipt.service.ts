218eb0e920e0fa811fbd93138de908c9cbadb77f
Larry Faudree Jr <lfaudreejr@hotmail.com>
2018-02-12 18:14:04 -0500

    Revert "Bugfix/update every item in receipt"

--- client/app/core/receipt.service.ts
+++ client/app/core/receipt.service.ts
@@ -11,6 +11,9 @@ export class ReceiptService {
     constructor(private http: HttpClient) {}
 
     completeReceipt(receipt) {
-        return this.http.post('/api/receipts/', receipt).subscribe(data => data);
+        console.log('complete receipt method fired!');
+        console.log('receipt is: ', receipt);
+        // this.httpClient.post(`/api/receipts/new`, receipt);
+        return this.http.post('/api/receipts/', receipt).subscribe(data => console.log('data is' + data));
     }
 }

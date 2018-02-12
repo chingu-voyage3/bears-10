6b43ca389be2fc5c2a729aa8bb895be48d5c9c32
Louis Heimel <louisheimel1@gmail.com>
2018-02-12 14:20:11 -0600

    update receipt

--- server/actions/receipt.ts
+++ server/actions/receipt.ts
@@ -2,17 +2,18 @@ import { Item } from '../models/item';
 import { Request, Response } from 'express';
 
 function createReceipt(req: Request, res: Response) {
-  console.log('creating receipt!')
-  console.log('req.body is: ' + JSON.stringify(req.body));
-  res.json('receipt created!')
-  const itemId    = req.body[0].id,
-        itemCount = req.body[0].count;
+  function updateCount(item) {
+    const itemId    = item.id,
+          itemCount = item.count;
 
-  Item.findById(itemId, (err, item) => {
-      if (err) { return console.log(err); }
-      item.count = Math.max(item.count - itemCount, 0);
-      item.save();
-  })
+    Item.findById(itemId, (err, item) => {
+        if (err) { return console.log(err); }
+        item.count = Math.max(item.count - itemCount, 0);
+        item.save()
+    })
+  }
+  req.body.forEach(updateCount);
+  res.json('receipt created!')
 } 
  export {
     createReceipt,
